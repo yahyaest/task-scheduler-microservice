f620adfae5d441f436dcc2cc6c53f238f828cca0
yahya <yahya.machat@digitalberry.fr>
2024-04-10 15:21:39 +0100

    Implement Task Admin in admin.py

--- task_scheduler_app/api/admin.py
+++ task_scheduler_app/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+# admin.site.register(Task)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'status', 'creation_date', 'last_update', 'user')
+    list_filter = ('status', 'creation_date', 'last_update')
+    list_editable = ('user',)
+    search_fields = ('name', 'status')
+    ordering = ('-creation_date', '-last_update')
+    list_per_page = 10
